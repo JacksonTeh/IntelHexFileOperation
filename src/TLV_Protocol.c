7ddf562ce3dc2035cac35b56262bb2c8d4e7ef7e
JacksonTeh <jackson_dmc69@hotmail.com>
2014-09-10 22:43:18 +0800

    Realigned TLV_Protocol.h, TLV_Protocol.c, test_TLV_Protocol.c

--- src/TLV_Protocol.c
+++ src/TLV_Protocol.c
@@ -7,92 +7,91 @@
  * To request Programming Mode from the PIC module
  *
  * Input
- *		receiveByte		is the byte receive through RS232
+ *      receiveByte     is the byte receive through RS232
  *
  * Return
- *		0		to indicate Programming Mode is unable to request
- *		1		to indicate Programming Mode is able to request
+ *      0       to indicate Programming Mode is unable to request
+ *      1       to indicate Programming Mode is able to request
  */
 int requestProgrammingMode(char receiveByte)
 {
-	TLV *tlvMessage = createProgrammingMode();
+    TLV *tlvMessage = createProgrammingMode();
 
-	RS232_SendByte(COM_PORT, tlvMessage->type);
-	// Sleep(100);
+    RS232_SendByte(COM_PORT, tlvMessage->type);
 
-	RS232_PollComport(COM_PORT, &receiveByte, 1);
+    RS232_PollComport(COM_PORT, &receiveByte, 1);
 
-	deleteTLV(tlvMessage);
+    deleteTLV(tlvMessage);
 
-	if(receiveByte != ACK)
-		return 0;
+    if(receiveByte != ACK)
+        return 0;
 
-	return 1;
+    return 1;
 }
 
 /*
  * To read a hex line and then create Intel Hex16 Data
  *
  * Input
- *		hexFile		is a pointer to FILE object
- *		data		is a pointer to IntelHex16Data object
- *		address		is a pointer to two high byte of extented address
+ *      hexFile     is a pointer to FILE object
+ *      data        is a pointer to IntelHex16Data object
+ *      address     is a pointer to two high byte of extented address
  *
  * Return
- *		0		to indicate the end of the hex line
- *		1		to indicate there still some hex line left
- *		-1		to indicate nothing to return
+ *      0       to indicate the end of the hex line
+ *      1       to indicate there still some hex line left
+ *      -1      to indicate nothing to return
  */
 int readHexLineAndCreateIntelHex16Data(FILE *hexFile, IntelHex16Data *data, int *address)
 {
-	char buffer[1024], getType;
-	int dataHigh, dataLow;
+    char buffer[1024], getType;
+    int dataHigh, dataLow;
 
-	readLine(hexFile, buffer);
+    readLine(hexFile, buffer);
 
-	data = createIntelHex16Data(buffer);
+    data = createIntelHex16Data(buffer);
 
-	getType = getByteFromIntelHex16Data(data, 4);
+    getType = getByteFromIntelHex16Data(data, 4);
 
-	if(getType == 4)
-	{
-		dataHigh = getByteFromIntelHex16Data(data, 5);
-		dataLow = getByteFromIntelHex16Data(data, 6);
-		*address = (dataHigh << 8) | dataLow;
-		deleteIntelHex16Data(data);
-		return -1;
-	}
-	else if(getType == 1)
-	{
-		deleteIntelHex16Data(data);
-		return 0;
-	}
-	else
-		return 1;
+    if(getType == 4)
+    {
+        dataHigh = getByteFromIntelHex16Data(data, 5);
+        dataLow = getByteFromIntelHex16Data(data, 6);
+        *address = (dataHigh << 8) | dataLow;
+        deleteIntelHex16Data(data);
+        return -1;
+    }
+    else if(getType == 1)
+    {
+        deleteIntelHex16Data(data);
+        return 0;
+    }
+    else
+        return 1;
 }
 
 /*
  * To send data code to the PIC module to program
  *
  * Input
- *		tlv				is a pointer to TLV object
- *		receiveByte		is the byte receive through RS232
+ *      tlv             is a pointer to TLV object
+ *      receiveByte     is the byte receive through RS232
  *
  * Return
- *		0		to indicate NACK is receive from the PIC module
- *		1		to indicate the program is able to flash
+ *      0       to indicate NACK is receive from the PIC module
+ *      1       to indicate the program is able to flash
  */
 int sendDataCode(TLV *tlv, char receiveByte)
 {
-	sendTLV(tlv);
-	Sleep(100);
+    sendTLV(tlv);
+    Sleep(100);
 
-	RS232_PollComport(COM_PORT, &receiveByte, 1);
+    RS232_PollComport(COM_PORT, &receiveByte, 1);
 
-	if(receiveByte != ACK)
-		return 0;
+    if(receiveByte != ACK)
+        return 0;
 
-	return 1;
+    return 1;
 }
 
 /*
@@ -100,64 +99,64 @@ int sendDataCode(TLV *tlv, char receiveByte)
  */
 void requestStartRunningMode(void)
 {
-	TLV *tlvMessage = createStartRunningMessage();
+    TLV *tlvMessage = createStartRunningMessage();
 
-	RS232_SendByte(COM_PORT, tlvMessage->type);
+    RS232_SendByte(COM_PORT, tlvMessage->type);
 
-	deleteTLV(tlvMessage);
+    deleteTLV(tlvMessage);
 }
 
 int tlvProtocol()
 {
-	int address = 0x0000, programmingModeStatus, sendStatus, result, i = 0;
-	char receiveByte;
-	FILE *hexFile;
-	IntelHex16Data *data;
-	TLV *tlvMessage;
+    int address = 0x0000, programmingModeStatus, sendStatus, result, i = 0;
+    char receiveByte;
+    FILE *hexFile;
+    IntelHex16Data *data;
+    TLV *tlvMessage;
 
-	hexFile = fopen(IntelHexFile, "r");
+    hexFile = fopen(IntelHexFile, "r");
 
-	if(hexFile == NULL)
-	{
-		printf("error: cannot open the file LEDBlink.hex");
-		return 0;	// Terminate the program immediately if file unable to open
-	}
+    if(hexFile == NULL)
+    {
+        printf("error: cannot open the file LEDBlink.hex");
+        return 0;       // Terminate the program immediately if file unable to open
+    }
 
-	RS232_OpenComport(COM_PORT, 9600);
+    RS232_OpenComport(COM_PORT, 9600);
 
-	receiveByte = NACK;
-	do{
-		if(i == 3)
-			return 0;	// Terminate the program immediately if the programming mode is unable to set
+    receiveByte = NACK;
+    do{
+        if(i == 3)
+            return 0;   // Terminate the program immediately if the programming mode is unable to set
 
-		programmingModeStatus = requestProgrammingMode(receiveByte);
-		i++;
-	}while(!programmingModeStatus);		//repeat request again if the ACK is not receive
+        programmingModeStatus = requestProgrammingMode(receiveByte);
+        i++;
+    }while(!programmingModeStatus);     //repeat request again if the ACK is not receive
 
-	/* receiveByte = ACK;
-	do{
-		result = readHexLineAndCreateIntelHex16Data(hexFile, data, &address);
+    /* receiveByte = ACK;
+    do{
+        result = readHexLineAndCreateIntelHex16Data(hexFile, data, &address);
 
-		if(result == 1)
-		{
-			tlvMessage = createProgrammingMode(data, address);
+        if(result == 1)
+        {
+            tlvMessage = createProgrammingMode(data, address);
 
-			do{
-				sendStatus = sendDataCode(tlvMessage, receiveByte);
-			}while(!sendStatus);		//repeat sending the same tlv if the ACK is not receive
+            do{
+                sendStatus = sendDataCode(tlvMessage, receiveByte);
+            }while(!sendStatus);        //repeat sending the same tlv if the ACK is not receive
 
-			deleteIntelHex16Data(data);
-			deleteTLV(tlvMessage);
-		}
-	}while(result != 0);
+            deleteIntelHex16Data(data);
+            deleteTLV(tlvMessage);
+        }
+    }while(result != 0);
 
-	requestStartRunningMode();*/
+    requestStartRunningMode();*/
 
-	RS232_CloseComport(COM_PORT);
+    RS232_CloseComport(COM_PORT);
 
-	fclose(hexFile);
+    fclose(hexFile);
 	
-	return 1;
+    return 1;
 }
 
 /* 	RS232_SendByte(COM_PORT, 0x01);
--- test/test_TLV_Protocol.c
+++ test/test_TLV_Protocol.c
@@ -15,156 +15,156 @@ void tearDown(void)
 
 void test_requestProgrammingMode_should_return_0_if_NACK_is_receive_from_RS232(void)
 {
-	TLV *tlvMessage = createProgrammingMode();
-	char receiveByte = NACK;
-	int result;
+    TLV *tlvMessage = createProgrammingMode();
+    char receiveByte = NACK;
+    int result;
 
-	RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
-	RS232_PollComport_ExpectAndReturn(COM_PORT, &receiveByte, 1, 1);
+    RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
+    RS232_PollComport_ExpectAndReturn(COM_PORT, &receiveByte, 1, 1);
 
-	result = requestProgrammingMode(receiveByte);
+    result = requestProgrammingMode(receiveByte);
 
-	TEST_ASSERT_EQUAL(0, result);
+    TEST_ASSERT_EQUAL(0, result);
 
-	deleteTLV(tlvMessage);
+    deleteTLV(tlvMessage);
 }
 
 void test_requestProgrammingMode_should_return_1_if_ACK_is_receive_from_RS232(void)
 {
-	TLV *tlvMessage = createProgrammingMode();
-	char receiveByte = ACK;
-	int result;
+    TLV *tlvMessage = createProgrammingMode();
+    char receiveByte = ACK;
+    int result;
 
-	RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
-	RS232_PollComport_ExpectAndReturn(COM_PORT, &receiveByte, 1, 1);
+    RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
+    RS232_PollComport_ExpectAndReturn(COM_PORT, &receiveByte, 1, 1);
 
-	result = requestProgrammingMode(receiveByte);
+    result = requestProgrammingMode(receiveByte);
 
-	TEST_ASSERT_EQUAL(1, result);
+    TEST_ASSERT_EQUAL(1, result);
 
-	deleteTLV(tlvMessage);
+    deleteTLV(tlvMessage);
 }
 
 void test_readHexLineAndCreateIntelHex16Data_should_return_address_if_the_type_in_hex_line_is_04(void)
 {
-	FILE *hexFile;
-	IntelHex16Data *data;
-	int result, address = 0x0000;
+    FILE *hexFile;
+    IntelHex16Data *data;
+    int result, address = 0x0000;
 
-	hexFile = fopen("test/data/Test02.hex", "r");
+    hexFile = fopen("test/data/Test02.hex", "r");
 
-	if(hexFile == NULL)
-	{
-		printf("error: cannot open the file Test.hex");
-		exit(-1);
-	}
+    if(hexFile == NULL)
+    {
+        printf("error: cannot open the file Test.hex");
+        exit(-1);
+    }
 
-	result = readHexLineAndCreateIntelHex16Data(hexFile, data, &address);
+    result = readHexLineAndCreateIntelHex16Data(hexFile, data, &address);
 
-	TEST_ASSERT_EQUAL(0x0030, address);
-	TEST_ASSERT_EQUAL(-1, result);
+    TEST_ASSERT_EQUAL(0x0030, address);
+    TEST_ASSERT_EQUAL(-1, result);
 }
 
 void test_readHexLineAndCreateIntelHex16Data_should_return_0_if_the_type_in_hex_line_is_01(void)
 {
-	FILE *hexFile;
-	IntelHex16Data *data;
-	int result, address = 0x0000;
+    FILE *hexFile;
+    IntelHex16Data *data;
+    int result, address = 0x0000;
 
-	hexFile = fopen("test/data/Test03.hex", "r");
+    hexFile = fopen("test/data/Test03.hex", "r");
 
-	if(hexFile == NULL)
-	{
-		printf("error: cannot open the file Test.hex");
-		exit(-1);
-	}
+    if(hexFile == NULL)
+    {
+        printf("error: cannot open the file Test.hex");
+        exit(-1);
+    }
 
-	result = readHexLineAndCreateIntelHex16Data(hexFile, data, &address);
+    result = readHexLineAndCreateIntelHex16Data(hexFile, data, &address);
 
-	TEST_ASSERT_EQUAL(0, address);
-	TEST_ASSERT_EQUAL(0, result);
+    TEST_ASSERT_EQUAL(0, address);
+    TEST_ASSERT_EQUAL(0, result);
 }
 
 void test_readHexLineAndCreateIntelHex16Data_should_return_1_if_the_type_in_hex_line_is_other_than_04_and_01(void)
 {
-	FILE *hexFile;
-	IntelHex16Data *data;
-	int result, address = 0x0000;
+    FILE *hexFile;
+    IntelHex16Data *data;
+    int result, address = 0x0000;
 
-	hexFile = fopen("test/data/Test04.hex", "r");
+    hexFile = fopen("test/data/Test04.hex", "r");
 
-	if(hexFile == NULL)
-	{
-		printf("error: cannot open the file Test.hex");
-		exit(-1);
-	}
+    if(hexFile == NULL)
+    {
+        printf("error: cannot open the file Test.hex");
+        exit(-1);
+    }
 
-	result = readHexLineAndCreateIntelHex16Data(hexFile, data, &address);
+    result = readHexLineAndCreateIntelHex16Data(hexFile, data, &address);
 
-	TEST_ASSERT_EQUAL(0, address);
-	TEST_ASSERT_EQUAL(1, result);
-	deleteIntelHex16Data(data);
+    TEST_ASSERT_EQUAL(0, address);
+    TEST_ASSERT_EQUAL(1, result);
+    deleteIntelHex16Data(data);
 }
 
 void test_sendDataCode_should_return_1_if_ACK_is_receive_from_RS232(void)
 {
-	char buffer[1024] = ":10000000020E732745187327451873274518732761\n", receiveByte = ACK;
-	int address = 0x0030, i, result;
-	IntelHex16Data *data = createIntelHex16Data(buffer);
-	TLV *tlvMessage = createProgramMessage(data, address);
+    char buffer[1024] = ":10000000020E732745187327451873274518732761\n", receiveByte = ACK;
+    int address = 0x0030, i, result;
+    IntelHex16Data *data = createIntelHex16Data(buffer);
+    TLV *tlvMessage = createProgramMessage(data, address);
 
-	RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
-	RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->length, 0);
+    RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
+    RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->length, 0);
 
-	for(i = 0; i < tlvMessage->length; i++)
-		RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->value[i], 0);
+    for(i = 0; i < tlvMessage->length; i++)
+        RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->value[i], 0);
 
-	RS232_PollComport_ExpectAndReturn(COM_PORT, &receiveByte, 1, 1);
+    RS232_PollComport_ExpectAndReturn(COM_PORT, &receiveByte, 1, 1);
 
-	result = sendDataCode(tlvMessage, receiveByte);
+    result = sendDataCode(tlvMessage, receiveByte);
 
-	TEST_ASSERT_EQUAL(1, result);
-	deleteIntelHex16Data(data);
-	deleteTLV(tlvMessage);
+    TEST_ASSERT_EQUAL(1, result);
+    deleteIntelHex16Data(data);
+    deleteTLV(tlvMessage);
 }
 
 void test_sendDataCode_should_return_0_if_NACK_is_receive_from_RS232(void)
 {
-	char buffer[1024] = ":10000000020E732745187327451873274518732761\n", receiveByte = NACK;
-	int address = 0x0030, i, result;
-	IntelHex16Data *data = createIntelHex16Data(buffer);
-	TLV *tlvMessage = createProgramMessage(data, address);
+    char buffer[1024] = ":10000000020E732745187327451873274518732761\n", receiveByte = NACK;
+    int address = 0x0030, i, result;
+    IntelHex16Data *data = createIntelHex16Data(buffer);
+    TLV *tlvMessage = createProgramMessage(data, address);
 
-	RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
-	RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->length, 0);
+    RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
+    RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->length, 0);
 
-	for(i = 0; i < tlvMessage->length; i++)
-		RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->value[i], 0);
+    for(i = 0; i < tlvMessage->length; i++)
+        RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->value[i], 0);
 
-	RS232_PollComport_ExpectAndReturn(COM_PORT, &receiveByte, 1, 1);
+    RS232_PollComport_ExpectAndReturn(COM_PORT, &receiveByte, 1, 1);
 
-	result = sendDataCode(tlvMessage, receiveByte);
+    result = sendDataCode(tlvMessage, receiveByte);
 
-	TEST_ASSERT_EQUAL(0, result);
-	deleteIntelHex16Data(data);
-	deleteTLV(tlvMessage);
+    TEST_ASSERT_EQUAL(0, result);
+    deleteIntelHex16Data(data);
+    deleteTLV(tlvMessage);
 }
 
 void test_requestStartRunningMode_should_send_the_start_running_message_through_serial_transmit(void)
 {
-	TLV *tlvMessage = createStartRunningMessage();
+    TLV *tlvMessage = createStartRunningMessage();
 
-	RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
+    RS232_SendByte_ExpectAndReturn(COM_PORT, tlvMessage->type, 0);
 
-	requestStartRunningMode();
+    requestStartRunningMode();
 
-	deleteTLV(tlvMessage);
+    deleteTLV(tlvMessage);
 }
 /* 
 void test_tlvProtocol(void)
 {
-	int result;
+    int result;
 	
-	result = tlvProtocol();
-	TEST_ASSERT_EQUAL(1, result);
+    result = tlvProtocol();
+    TEST_ASSERT_EQUAL(1, result);
 }*/
